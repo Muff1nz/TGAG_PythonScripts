2a46285c0b3bd56215ed1710664aaeaf5e593409
Muff1nz <muffinz314@gmail.com>
2018-03-10 23:40:06 +0100

    Added support for labels

--- RealBenchGrapher.py
+++ RealBenchGrapher.py
@@ -7,11 +7,16 @@ print("Provided file: " + sys.argv[1])
 file = open(sys.argv[1], 'r')
 inGraph = False
 graphs = []
+labels = []
+plots = []
 
 fpsMax = 0
 timeMax = 0
 
 for line in file:
+    if line.startswith("label:"):
+        labels.append(parse.parse("label: {}", line)[0])
+
     if line.startswith("["):
         graphs.append([[], []])
         inGraph = True
@@ -29,14 +34,24 @@ for line in file:
 
     elif inGraph:
         parsed = parse.parse("x:{}|y:{}", line)
-        graphs[len(graphs) - 1][0].append(float(parsed[0]))
-        graphs[len(graphs) - 1][1].append(float(parsed[1]))
+        if float(parsed[0]) >= 60:
+            graphs[len(graphs) - 1][0].append(float(parsed[0]))
+            graphs[len(graphs) - 1][1].append(float(parsed[1]))
 
+i = 0
 for graph in graphs:
-    plt.plot(graph[0], graph[1])
+    if len(labels) > 0:
+        plots.append(plt.plot(graph[0], graph[1], label=labels[i]))
+    else:
+        plots.append(plt.plot(graph[0], graph[1]))
+    i += 1
+
+if len(labels) > 0:
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+               ncol=2, mode="expand", borderaxespad=0.)
 
 
-plt.axis([0, timeMax + 5, 0, fpsMax + 10])
+plt.axis([55, timeMax + 5, 0, fpsMax + 10])
 plt.ylabel("FPS")
 plt.xlabel("TIME")
 plt.show()
